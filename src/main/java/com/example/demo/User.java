64b4607503789d6063df469b3de2c6278e0f731d
Home245 <148992841+Home245@users.noreply.github.com>
2023-12-13 22:57:41 +0300

    Update User.java

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,12 +1,15 @@
 package com.example.demo;
-
 public class User {
-    private String name;
     private int age;
+    private String username;
+    private String password;
+    private String id;
 
-    public User(int age, String name) {
-        this.name = name;
+    public User(String name, String password, String id, int age) {
         this.age = age;
+        this.username = name;
+        this.password = password;
+        this.id=id;
     }
 
     public int getAge() {
@@ -17,11 +20,27 @@ public class User {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String name) {
+        this.username = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
